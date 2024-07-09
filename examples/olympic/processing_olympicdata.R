97eae0fc2230f8afeebcaf748e90b0e33bd93d46
fmeulen <frankvdm0@gmail.com>
2024-07-09 14:43:20 +0200

    working analysis!

--- examples/olympic/processing_olympicdata.R
+++ examples/olympic/processing_olympicdata.R
@@ -24,3 +24,13 @@ olymp %>% summarise(m=mean(Sport, na.rm=TRUE), std=sd(Sport, na.rm=TRUE))
 olymp %>% summarise(m=mean(Strength, na.rm=TRUE), std=sd(Strength, na.rm=TRUE))
 olymp %>% summarise(m=mean(Competition, na.rm=TRUE), std=sd(Competition, na.rm=TRUE))
 
+
+Covariates <- read_csv("Covariates.csv")
+mm <- t(as.matrix(Covariates))
+mm[,2] <- (mm[,2]-10.2)/5.12
+mm[,3] <- (mm[,3]-3.45)/2.12
+mm[,4] <- (mm[,4]-1.89)/1.91
+
+Covariates_standardised <- tibble(mm)
+
+write.csv(Covariates_standardised, "Covariates_standardised.csv", row.names=FALSE)
